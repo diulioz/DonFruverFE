e4c087cbc43a5bf4f11ca9fcf00e68d498610da4
David Criollo <diulioz.u@gmail.com>
2023-08-01 11:08:25 -0500

    CarroCompleto

--- src/app/carro/carro.component.ts
+++ src/app/carro/carro.component.ts
@@ -19,25 +19,30 @@ export class CarroComponent implements OnInit {
   pedidos: PedidoModel[] = [];
   usuarios: UsuarioModel[]=[];
   pedidoConfirmado: Map<string, boolean> = new Map<string, boolean>();
-  idUsuario: number = 0;
 
   constructor(private detalleService: DetallePedidoService, 
     private pedidoService: PedidoService, 
     private usuarioService: UsuarioService) { }
 
   ngOnInit() {
-    const idUsuario = Number(localStorage.getItem('id'));
+    const idUsuario = localStorage.getItem('id');
     this.detalles = this.detalleService.obtenerDetalles();
     this.obtenerProductos();
-
+    const prueba = this.esUsuario(idUsuario)
+    console.log(prueba)
     // Obtener los pedidos del usuario
     this.pedidoService.obtenerPedidos().subscribe((pedidos) => {
       this.pedidos = pedidos; // Asignamos los pedidos a la lista pedidos
+      // const prueba = this.esUsuario()
+    console.log(prueba)
       console.log('Lista de pedidos:', this.pedidos);
       for (const pedido of pedidos) {
+        const id = pedido.Usuario_ID
+        // const prueba = this.esUsuario(pedido.Usuario_ID)
         this.pedidoConfirmado.set(pedido.idPedido, pedido.Confirmado === 1 || pedido.Confirmado === 0);
       }
     });
+  
     // this.pedidoService.obtenerPedidos().subscribe((pedidos) => {
     //   this.pedidos = pedidos; // Asignamos los pedidos a la lista pedidos
     //   console.log('Lista de pedidos:', this.pedidos);
@@ -53,40 +58,45 @@ export class CarroComponent implements OnInit {
       // }
     });
   }
-
+  //FUNCIONES PARA OBTENER DATOS DE LOS PRODUCTOS
+  //se obtiene la totalidad de los productos
   obtenerProductos() {
     this.detalleService.obtenerProductos().subscribe((productos) => {
       this.productos = productos;
     });
   }
-
-  // obtenerPedidos() {
-  //   this.pedidoService.obtenerPedidos().subscribe((pedidos) => {
-  //     this.pedidos = pedidos;
-  //   });
-  // }
-
+  //se obtiene el nombre del producto usande el idProducto
   obtenerNombreProducto(idProducto: string): string {
     const productoEncontrado = this.productos.find(producto => producto.idProducto === idProducto);
     return productoEncontrado ? productoEncontrado.Nombre : 'Producto no encontrado';
   }
-
-  
+  //Se obtiene el precio del producto usando el idProducto
   obtenerPrecioProducto(idProducto: string): number {
     const productoEncontrado = this.productos.find(producto => producto.idProducto === idProducto);
     return productoEncontrado ? productoEncontrado.Precio : 0; // Retorna 0 si no se encuentra el producto
   }
 
+  //Permite que en el carrito solo se muestren los pedidos del usuario en sesión
+  esUsuario(idUsuario: string|null){
+    const usuario1 = Number(localStorage.getItem('id'));
+    const usuario2 = Number(idUsuario)
+    if(usuario1==usuario2){
+      return true;
+    }
+    else{
+      return false;
+    }
+  }
+  //Permite obtener el Usuario_ID en pedido, usando el idPedido
   obtenerUPedido(idPedido: string): string {
     const pedidoEncontrado = this.pedidos.find(pedido => pedido.idPedido === idPedido);
-    return pedidoEncontrado ? pedidoEncontrado.Usuario_ID.toString() : 'UN';
+    return pedidoEncontrado ? pedidoEncontrado.Usuario_ID.toString() : 'No encontrado';
   }
-
+  //Permite obtener el Nombre del Usuario 
   obtenerNombre(Usuario_ID: string): string {
-    console.log('Contenido del arreglo usuarios:', this.usuarios);
-    const pedidoEncontrado = this.usuarios.find(usuario => usuario.idUsuario === Usuario_ID);
-    console.log('Resultado de la función obtenerNombre:', pedidoEncontrado);
-    return pedidoEncontrado ? pedidoEncontrado.Nombre : 'UN';
+    const aux = Number(Usuario_ID)
+    const pedidoEncontrado = this.usuarios.find(usuario => Number(usuario.idUsuario) == aux);
+    return pedidoEncontrado ? pedidoEncontrado.Nombre : 'No encontrado';
   }
 
   obtenerNombreUsuario(idPedido: string): string {
